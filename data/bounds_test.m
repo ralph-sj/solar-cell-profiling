c96140e0d42da1ee443caf91c312457dd5000485
Ralph Stevenson-Jones <ralph.stevenson-jones@manchester.ac.uk>
2016-11-21 22:16:52 +0000

    amended bounds_test.m
    updated upper and lower bounds :

--- data/bounds_test.m
+++ data/bounds_test.m
@@ -1,9 +1,9 @@
-intervals = 5;
+intervals = 50;
 i = 1:intervals;
 range = floor((max(light)-min(light)/intervals)); % light range per interval
 
-lower_bound = i*(max(light)-min(light))/intervals;
-upper_bound = (i+1)*(max(light)-min(light))/intervals;
+lower_bound = (i-1)*(max(light)-min(light))/intervals;
+upper_bound = i*(max(light)-min(light))/intervals;
 
 for iy = 1:intervals;
     ix = find (and((light<upper_bound(iy)),(light>=lower_bound(iy))));    % finds light values within that range
