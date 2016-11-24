61e39195ddfd6c37405833ce0f32f7603ea8cdaf
Ralph Stevenson-Jones <ralph.stevenson-jones@manchester.ac.uk>
2016-11-24 15:56:41 +0000

    updated bounds_test
    changed calculation of midpoints to midpoints of each data range c.f.
    average of all values
    included code to save summarised light and power light and power values
    to .mat files with the name of the file in the workspace

--- analysis/bounds_test.m
+++ analysis/bounds_test.m
@@ -1,16 +1,30 @@
-intervals = 10;
-i = 1:intervals;
-range = floor((max(light)-min(light)/intervals)); % light range per interval
+clear lower_bound upper_bound p_av l_av
 
-lower_bound = min(light) + (i-1)*(max(light)-min(light))/intervals;
-upper_bound = min(light) + i*(max(light)-min(light))/intervals;
+step_size = 25; % step size (in lux)
+min_light = 0; % minimum lux
+max_light = floor(max(light)/step_size) * step_size; % the maximum light level as an integer multiple of the step size
+intervals = max_light/step_size; % number of itervals
+i = 1:intervals;
+
+lower_bound = min_light + (i-1)*(max_light-min_light)/intervals;
+upper_bound = min_light + i*(max_light-min_light)/intervals;
+
+% exclude -infinity answers (only applicable for log functions) replace
+% with NaN
+II = find(Power == -Inf);
+Power(II) = NaN;
+light(II)=NaN;
+clear II
 
 for iy = 1:intervals;
     ix = find (and((light<upper_bound(iy)),(light>=lower_bound(iy))));    % finds light values within that range
-    p_av(iy) = mean(Power(ix));
+    p_av(iy) = (max(Power(ix))+min(Power(ix)))/2;
     l_av(iy) = (lower_bound(iy)+upper_bound(iy))/2;
 end
 
 hold on
 plot(l_av,p_av, 'r*')
 hold off
+
+% save to workspace
+save(file, 'l_av', 'p_av');
